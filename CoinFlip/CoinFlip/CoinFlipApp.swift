f2d2dfe6f72e0887486341904c19292d38860dbb
Olivia Lopes <olivialopes@tamu.edu>
2024-03-20 09:10:30 -0500

    add to github

---
+++ CoinFlip/CoinFlip/CoinFlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinFlipApp.swift
+//  CoinFlip
+//
+//  Created by Lopes, Olivia on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinFlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinFlip/CoinFlip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoinFlip
+//
+//  Created by Lopes, Olivia on 3/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
